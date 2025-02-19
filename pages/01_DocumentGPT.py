f8f6095017b714c47ee80c39b1522da8da5f4f76
이현서 <slrkanjfkrn@gmail.com>
2025-02-19 13:39:31 +0900

    gpt study 7

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -4,16 +4,39 @@ from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.embeddings.cache import CacheBackedEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain_community.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
 
 import streamlit as st
-import nltk
-nltk.data.path.append("~/nltk_data")
+# import nltk
+# nltk.data.path.append("~/nltk_data")
+
+class ChatCallbackHandler(BaseCallbackHandler):
+
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token 
+        self.message_box.markdown(self.message)
+        
+
+llm = ChatOpenAI(temperature=0.1, streaming=True, callbacks=[ChatCallbackHandler()])
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📃",
 )
+st.title("DocumentGPT")
 
+@st.cache_resource(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -33,15 +56,56 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message,role)
+
+def save_message(message, role):
+    st.session_state["messages"].append({"messages": message, "role": role})
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["messages"], message["role"], save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+    """
+    Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up
+
+    Context: {context}
+    """),
+    ("human", "{question}")
+    ])
+
 st.markdown("""
 Welcome!
 
 Use this chatbot to ask questions to an AI about your files!
+            
+Upload your files on the sidebar.
 """)
 
-file = st.file_uploader("Upload a .txt .pdf or  .docx file", type =["pdf", "docx", "txt"])
+with st.sidebar:
+    file = st.file_uploader("Upload a .txt .pdf or  .docx file", type =["pdf", "docx", "txt"])
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+    send_message("I'm ready! Ask away!","ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about you file...")
+    if message:
+        send_message(message, "human")
+        chain = ({
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough()
+        } | prompt | llm)
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+
+else:
+    st.session_state["messages"] = []
